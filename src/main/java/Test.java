880b3d79df455ef320e8e6397929e36cc8125aa6
mq <meiqiaoJohn@outlook.com>
2018-06-10 16:50:33 +0800

    第五次修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("冲突");
 
-        System.out.println("test");
+        System.out.println("test we are new");
 
         System.out.println("change");
     }
